2708fd32adce0df0061479e090615bc6b1f9887a
sging <rhrnrkd135@gmail.com>
2024-06-09 16:52:04 +0900

    + test.js prettier setting

---
+++ src/components/test.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class test extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      first,
+    };
+  }
+  render() {
+    return <div>test</div>;
+  }
+}
+
+// pretier test mac book
